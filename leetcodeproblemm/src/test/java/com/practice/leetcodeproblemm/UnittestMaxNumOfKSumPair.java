46ca3545ef3ba58af6812895d8bfe673c63a9ed7
prave <prave@Praveen>
2024-01-09 21:42:27 +0530

    Initial Commit

--- leetcodeproblemm/src/test/java/com/practice/leetcodeproblemm/UnittestMaxNumOfKSumPair.java
+++ leetcodeproblemm/src/test/java/com/practice/leetcodeproblemm/UnittestMaxNumOfKSumPair.java
@@ -7,29 +7,8 @@ import org.junit.Test;
 public class UnittestMaxNumOfKSumPair {
 
 	@Test
-	public void test1() {
-		MaxNumberOfKSumPairstestt m = new MaxNumberOfKSumPairstestt();
-		int[] nums = { 3, 1, 1, 1 };
-		int k = 2;
-		int check = m.maxOperations(nums, k);
-		assertEquals(1, check);
+	public void test() {
+		fail("Not yet implemented");
 	}
 
-	@Test
-	public void test2() {
-		MaxNumberOfKSumPairstestt m = new MaxNumberOfKSumPairstestt();
-		int[] nums = { 1, 2, 3, 4, 5 };
-		int k = 5;
-		int check = m.maxOperations(nums, k);
-		assertEquals(2, check);
-	}
-
-	@Test
-	public void test3() {
-		MaxNumberOfKSumPairstestt m = new MaxNumberOfKSumPairstestt();
-		int[] nums = { 3, 1, 3, 4, 3 };
-		int k = 6;
-		int check = m.maxOperations(nums, k);
-		assertEquals(1, check);
-	}
 }
