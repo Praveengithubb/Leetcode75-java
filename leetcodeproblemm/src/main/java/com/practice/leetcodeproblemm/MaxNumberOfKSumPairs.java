c54c3455f1d5ee145e1e2b8359e7a1424bb0babf
prave <prave@Praveen>
2024-01-09 21:43:20 +0530

    Initial Commit

--- leetcodeproblemm/src/main/java/com/practice/leetcodeproblemm/MaxNumberOfKSumPairs.java
+++ leetcodeproblemm/src/main/java/com/practice/leetcodeproblemm/MaxNumberOfKSumPairs.java
@@ -1,37 +1,34 @@
 package com.practice.leetcodeproblemm;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.Arrays;
 
-public class MaxNumberOfKSumPairs {
-	 public static int maxOperations(int[] nums, int k) {
-		 int result=0;
-		 Arrays.sort(nums);
-		 int start=0;
-		 int end=nums.length-1;
-		 while(start<end)
-		 {
-			 if(nums[start]+nums[end]<k)
-			 {
-				 start++;
-			 }
-			 else if(nums[start]+nums[end]>k)
-			 {
-				 end--;
-			 }
-			 else
-			 {
-				 start++;
-				 end--;
-				 result++;
-			 }
-		 }
-		return result; 
-	 }
+import org.junit.Test;
 
-	 
-public static void main(String[] args) {
-	int[] nums= {3,1,3,4,3};
-	int k=6;
-	System.out.println(maxOperations(nums,k));
-}
+public class MaxNumberOfKSumPairs {
+	public static int maxOperations(int[] nums, int k) {
+		int result = 0;
+		Arrays.sort(nums);
+		int start = 0;
+		int end = nums.length - 1;
+		while (start < end) {
+			if (nums[start] + nums[end] < k) {
+				start++;
+			} else if (nums[start] + nums[end] > k) {
+				end--;
+			} else {
+				start++;
+				end--;
+				result++;
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		int[] nums = { 3, 1, 3, 4, 3 };
+		int k = 6;
+		System.out.println(maxOperations(nums, k));
+	}
 }
--- leetcodeproblemm/src/test/java/com/practice/leetcodeproblemm/UnittestMaxNumOfKSumPair.java
+++ leetcodeproblemm/src/test/java/com/practice/leetcodeproblemm/UnittestMaxNumOfKSumPair.java
@@ -7,8 +7,27 @@ import org.junit.Test;
 public class UnittestMaxNumOfKSumPair {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test1() {
+		int[] nums = { 3, 1, 1, 1 };
+		int k = 2;
+		int check = MaxNumberOfKSumPairs.maxOperations(nums, k);
+		assertEquals(1, check);
 	}
 
+	@Test
+	public void test2() {
+		int[] nums = { 1, 2, 3, 4, 5 };
+		int k = 5;
+		int check = MaxNumberOfKSumPairs.maxOperations(nums, k);
+		assertEquals(2, check);
+	}
+
+	@Test
+	public void test3() {
+
+		int[] nums = { 3, 1, 3, 4, 3 };
+		int k = 6;
+		int check = MaxNumberOfKSumPairs.maxOperations(nums, k);
+		assertEquals(1, check);
+	}
 }
