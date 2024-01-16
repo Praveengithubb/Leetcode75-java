fcfcbcda1e3daab275d9371090c12eca84405f50
Praveengithubb <praveenraj2670@gmail.com>
2024-01-16 19:26:36 +0530

    Initial commit

--- leetcodeproblemm/src/test/java/com/practice/leetcodeproblemm/UnittestofUniqueNoOfOccurance.java
+++ leetcodeproblemm/src/test/java/com/practice/leetcodeproblemm/UnittestofUniqueNoOfOccurance.java
@@ -8,22 +8,24 @@ public class UnittestofUniqueNoOfOccurance {
 
 	@Test
 	public void test() {
-		UniqueNumberOfOccurance u =new UniqueNumberOfOccurance();
-		int [] arr = {1,2,2,1,1,3};
+		UniqueNumberOfOccurance u = new UniqueNumberOfOccurance();
+		int[] arr = { 1, 2, 2, 1, 1, 3 };
 		boolean check = u.uniqueOccurrences(arr);
 		assertEquals(check, true);
 	}
+
 	@Test
 	public void test1() {
-		UniqueNumberOfOccurance u =new UniqueNumberOfOccurance();
-		int [] arr = {1,2};
+		UniqueNumberOfOccurance u = new UniqueNumberOfOccurance();
+		int[] arr = { 1, 2 };
 		boolean check = u.uniqueOccurrences(arr);
 		assertEquals(check, false);
 	}
+
 	@Test
 	public void test2() {
-		UniqueNumberOfOccurance u =new UniqueNumberOfOccurance();
-		int [] arr = {-3,0,1,-3,1,1,1,-3,10,0};
+		UniqueNumberOfOccurance u = new UniqueNumberOfOccurance();
+		int[] arr = { -3, 0, 1, -3, 1, 1, 1, -3, 10, 0 };
 		boolean check = u.uniqueOccurrences(arr);
 		assertEquals(check, true);
 	}
