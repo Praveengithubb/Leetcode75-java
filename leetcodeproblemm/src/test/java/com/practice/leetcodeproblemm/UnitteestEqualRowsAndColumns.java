20b6b393f5c1b3ed4dfb03930336c25db4e349c3
Praveengithubb <praveenraj2670@gmail.com>
2024-01-18 00:32:49 +0530

    Initial commit

--- leetcodeproblemm/src/test/java/com/practice/leetcodeproblemm/UnitteestEqualRowsAndColumns.java
+++ leetcodeproblemm/src/test/java/com/practice/leetcodeproblemm/UnitteestEqualRowsAndColumns.java
@@ -8,22 +8,24 @@ public class UnitteestEqualRowsAndColumns {
 
 	@Test
 	public void test() {
-		EqualRowAndColumnPairs e =new EqualRowAndColumnPairs();
+		EqualRowAndColumnPairs e = new EqualRowAndColumnPairs();
 		int[][] grid = { { 3, 2, 1 }, { 1, 7, 6 }, { 2, 7, 7 } };
 		int check = e.equalPairs(grid);
 		assertEquals(check, 1);
 	}
+
 	@Test
 	public void test1() {
-		EqualRowAndColumnPairs e =new EqualRowAndColumnPairs();
-		int[][] grid = { { 3,1,2,2 }, { 1,4,4,5 }, { 2,4,2,2 } , {2,4,2,2}};
+		EqualRowAndColumnPairs e = new EqualRowAndColumnPairs();
+		int[][] grid = { { 3, 1, 2, 2 }, { 1, 4, 4, 5 }, { 2, 4, 2, 2 }, { 2, 4, 2, 2 } };
 		int check = e.equalPairs(grid);
 		assertEquals(check, 3);
 	}
+
 	@Test
 	public void test2() {
-		EqualRowAndColumnPairs e =new EqualRowAndColumnPairs();
-		int[][] grid = { { 13,13 }, { 13,13 }, };
+		EqualRowAndColumnPairs e = new EqualRowAndColumnPairs();
+		int[][] grid = { { 13, 13 }, { 13, 13 }, };
 		int check = e.equalPairs(grid);
 		assertEquals(check, 4);
 	}
